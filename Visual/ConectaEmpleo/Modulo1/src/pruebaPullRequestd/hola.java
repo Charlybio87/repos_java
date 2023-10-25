d08baf7e143e87467e9c3b5e6657a611f9f8ab8a
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2023-10-25 11:28:05 -0300

    Update hola.java
    agregar main

--- Visual/ConectaEmpleo/Modulo1/src/pruebaPullRequestd/hola.java
+++ Visual/ConectaEmpleo/Modulo1/src/pruebaPullRequestd/hola.java
@@ -1,5 +1,7 @@
 package pruebaPullRequestd;
 
 public class hola {
-    
+    public static void main(String[] args) {
+        System.out.println("Hola queriddo que querese que hagamos");
+    }
 }
